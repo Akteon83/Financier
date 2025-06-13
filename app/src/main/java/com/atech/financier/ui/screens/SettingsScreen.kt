25201e26238030e4ed50621afa7f443d8d1e9529
Akteon83 <akteon.akteonov@gmail.com>
2025-06-14 00:28:53 +0300

    Homework 1 Commit

--- app/src/main/java/com/atech/financier/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/atech/financier/ui/screens/SettingsScreen.kt
@@ -2,9 +2,12 @@ package com.atech.financier.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -18,20 +21,45 @@ import com.atech.financier.ui.theme.FinancierTheme
 @Composable
 private fun SettingsScreenPreview() {
     FinancierTheme {
-        RevenuesScreen()
+        SettingsScreen()
     }
 }
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(
+    darkThemeChecked: Boolean = false,
+    onDarkThemeCheckedChange: (Boolean) -> Unit = {},
+) {
+    val settingsItems = listOf(
+        "Основной цвет",
+        "Звуки",
+        "Хаптики",
+        "Код пароль",
+        "Cинхронизация",
+        "Язык",
+        "О программе"
+    )
     LazyColumn(
         modifier = Modifier.background(MaterialTheme.colorScheme.outlineVariant),
+        contentPadding = PaddingValues(bottom = 1.dp),
         verticalArrangement = Arrangement.spacedBy(1.dp)
     ) {
         item {
             ColumnItem(
-                modifier = Modifier.background(MaterialTheme.colorScheme.secondary),
-                title = "Основной цвет",
+                title = "Тёмная тема",
+                iconRight = {
+                    Switch(
+                        checked = darkThemeChecked,
+                        onCheckedChange = {
+                            onDarkThemeCheckedChange(it)
+                        }
+                    )
+                }
+            )
+        }
+        items(settingsItems) { item ->
+            ColumnItem(
+                title = item,
                 iconRight = {
                     Icon(
                         painter = painterResource(R.drawable.arrow_right),
---
+++ app/src/main/java/com/atech/financier/ui/viewmodels/ItemsViewModel.kt
@@ -0,0 +1,52 @@
+package com.atech.financier.ui.viewmodels
+
+import androidx.compose.runtime.mutableStateListOf
+import com.atech.financier.domain.models.Category
+
+class ItemsViewModel {
+
+    val items = mutableStateListOf<Category>(
+        Category(
+            id = 0,
+            name = "Аренда Квартиры",
+            emoji = "\uD83C\uDFE0",
+            isIncome = false,
+        ),
+        Category(
+            id = 1,
+            name = "Одежда",
+            emoji = "\uD83D\uDC57",
+            isIncome = false,
+        ),
+        Category(
+            id = 2,
+            name = "На собачку",
+            emoji = "\uD83D\uDC36",
+            isIncome = false,
+        ),
+        Category(
+            id = 3,
+            name = "Ремонт квартиры",
+            emoji = "РК",
+            isIncome = false,
+        ),
+        Category(
+            id = 4,
+            name = "Продукты",
+            emoji = "\uD83C\uDF6D",
+            isIncome = false,
+        ),
+        Category(
+            id = 5,
+            name = "Спортзал",
+            emoji = "\uD83C\uDFCB\uFE0F",
+            isIncome = false,
+        ),
+        Category(
+            id = 6,
+            name = "Медицина",
+            emoji = "\uD83D\uDC8A",
+            isIncome = false,
+        ),
+    )
+}
